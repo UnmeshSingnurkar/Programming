5b986511d1ea7238ed93d98609970eca41b3937f
Unmesh Singnurkar <singnurkaru06@gmail.com>
2026-06-15 19:55:40 +0530

    Add files via upload

---
+++ Python_Programming/Competitive/program1.py
@@ -0,0 +1,9 @@
+def main():
+    No = 11
+
+    print(No)
+    print(type(No))
+    print(id(No))
+
+if __name__ == "__main__" :
+    main()
---
+++ Python_Programming/Competitive/program2.py
@@ -0,0 +1,14 @@
+def main():
+    print("Enter first number : ")
+    No1 = int(input())
+
+    print("Enter second number : ")
+    No2 = int(input())
+
+    print("Addition is : ",No1 + No2)
+    print("Substraction is : ",No1 - No2)
+    print("Multiplication is : ",No1 * No2)
+    print("Division is : ",No1 / No2)
+
+if __name__ == "__main__" :
+    main()
