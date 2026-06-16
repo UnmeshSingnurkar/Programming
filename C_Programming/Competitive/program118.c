07921bbb1c0be5e525150f94ead7afadafd2bc17
Unmesh Singnurkar <singnurkaru06@gmail.com>
2026-06-16 15:53:35 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program118.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCount = 0;
+    char ch = 'a';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(j = 1, iCount = 1; j <= iCol; j++, iCount++)
+            {
+                printf("%d\t",iCount);
+            }
+            printf("\n");
+        }
+        else
+        {
+            for(j = 1, ch = 'a'; j <= iCol; j++, ch++)
+            {
+                printf("%c\t",ch);
+            }
+            printf("\n");
+        } 
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
---
+++ C_Programming/Competitive/program135.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCount = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\nEnter same number of rows and Columns");
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j || i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n"); 
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
